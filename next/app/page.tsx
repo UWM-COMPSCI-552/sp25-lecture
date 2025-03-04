d4e66ee827f361e3a895cfe6d2228c175683e67a
John T Boyland <boyland@uwm.edu>
2025-03-03 18:40:35 -0600

    After lecture 3/3

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,7 +1,27 @@
+import { Provider } from "@/components/ui/provider";
+import { ChakraProvider, HStack, Icon, Input, List, Text, VStack } from "@chakra-ui/react";
+import { MdAdd, MdDelete } from "react-icons/md";
+// import { DeleteIcon, AddIcon } from "@chakra-ui/icons";
+
 export const metadata = {
   title: "App Router",
 };
 
 export default function Page() {
-  return <h1>App Router</h1>;
+  return <VStack>
+    <Text>TODO List</Text>
+    <List.Root>
+      <List.Item>
+        <List.Indicator asChild color="red.500"><MdDelete/></List.Indicator>
+        Wash dishes
+      </List.Item>
+      <List.Item>
+      <List.Indicator asChild color="red.500">
+        <MdDelete />
+      </List.Indicator>
+      Do CS 552 homework #4
+      </List.Item>
+    </List.Root>
+    <HStack> <Input id="newitemid"/>  <Icon> <MdAdd/> </Icon></HStack>
+  </VStack>;
 }
--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,6 +1,8 @@
 import "@/styles/global.css";
 import type { AppProps } from "next/app";
+import { Provider } from "@/components/ui/provider";
+
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Provider><Component {...pageProps} /></Provider>;
 }
